99a213c09321b4210d4363fc4eb811f3828f913d
grogg19 <differ@list.ru>
2021-10-07 01:58:55 +0300

    обновлен массив lang

--- plugins/grogg19/projects/controllers/ProjectsController.php
+++ plugins/grogg19/projects/controllers/ProjectsController.php
@@ -1,23 +1,45 @@
 <?php namespace Grogg19\Projects\Controllers;
 
+use Backend\Behaviors\FormController;
+use Backend\Behaviors\ListController;
+use Backend\Behaviors\RelationController;
+use Backend\Behaviors\ReorderController;
 use Backend\Classes\Controller;
 use BackendMenu;
+use Lang;
 
 class ProjectsController extends Controller
 {
     public $implement = [
-        'Backend\Behaviors\ListController',
-        'Backend\Behaviors\FormController',
-        'Backend\Behaviors\ReorderController'
+        ListController::class,
+        FormController::class,
+        ReorderController::class,
+        RelationController::class
     ];
     
     public $listConfig = 'config_list.yaml';
     public $formConfig = 'config_form.yaml';
     public $reorderConfig = 'config_reorder.yaml';
+    public $relationConfig = 'config_relation.yaml';
 
     public function __construct()
     {
         parent::__construct();
         BackendMenu::setContext('Grogg19.Projects', 'main-menu-projects');
+    }
+
+    /**
+     *
+     * Returns path to reorder current album
+     *
+     * @return string
+     */
+    protected function getReorderPath() {
+        if (!isset($this->vars['formModel']->id)) {
+            return '';
+        }
+
+        $uri = \Backend::url('grogg19/projects/reorder/project/' . $this->vars['formModel']->id);
+        return $uri;
     }
 }
--- plugins/grogg19/projects/lang/ru/lang.php
+++ plugins/grogg19/projects/lang/ru/lang.php
@@ -1,6 +1,20 @@
 <?php return [
     'plugin' => [
-        'name' => 'Projects',
-        'description' => 'Каталог проектов'
+        'name' => 'Портфолио проектов',
+        'description' => 'Каталог проектов',
+        'project' => 'Проект',
+        'projects' => 'Каталог проектов',
+        'set_front_button' => 'Сделать главным изображением',
+        'reorder_button' => 'Упорядочить',
+        'title_label' => 'Название проекта',
+        'description_label' => 'Описание проекта'
+    ],
+    'messages' => [
+        'set_front' => 'Сделать главным изображением проекта?'
+    ],
+    'relation' => [
+        'create_name' => 'Завести новое изображение',
+        'delete' => 'Удалить',
+        'delete_confirm' => 'Удалить изображение?'
     ]
 ];
