2ed0b0b1c656e53388ed359e1782d4624ea87091
grogg19 <differ@list.ru>
2022-06-07 00:33:04 +0300

    добавлены новые поля в lang

--- plugins/grogg19/projects/lang/ru/lang.php
+++ plugins/grogg19/projects/lang/ru/lang.php
@@ -9,18 +9,25 @@
         'title_label' => 'Название проекта',
         'description_label' => 'Описание проекта',
         'front_label' => 'Главное изображение',
+        'tab' => 'Каталог проектов',
+        'access_permission' => 'Настройки доступа',
+        'manage_projects' => 'Управлять проектами',
     ],
     'messages' => [
-        'set_front' => 'Сделать главным изображением проекта?'
+        'set_front' => 'Сделать главным изображением проекта?',
     ],
     'relation' => [
         'create_name' => 'Завести новое изображение',
         'delete' => 'Удалить',
-        'delete_confirm' => 'Удалить изображение?'
+        'delete_confirm' => 'Удалить изображение?',
     ],
     'errors' => [
         'project_not_found' => 'Проект не найден',
         'cant_find_selected' => 'Выбранное фото не найдено',
-        'not_this_project' => 'Не в этом проекте'
-    ]
+        'not_this_project' => 'Не в этом проекте',
+    ],
+    'components' => [
+        'projects_list' => 'Список проектов',
+        'projects_list_description' => 'Каталог выполненных проектов',
+    ],
 ];
