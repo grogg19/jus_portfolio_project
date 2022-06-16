e29f8d8df9b1804cb43e54f814fa5f9d69a16c3c
grogg19 <differ@list.ru>
2022-06-16 21:12:25 +0300

    feat(lang): Добавил файл описания в английской версии

---
+++ plugins/grogg19/projects/lang/en/lang.php
@@ -0,0 +1,33 @@
+<?php return [
+    'plugin' => [
+        'name' => 'Портфолио проектов',
+        'description' => 'Каталог проектов',
+        'project' => 'Проект',
+        'projects' => 'Каталог проектов',
+        'set_front_button' => 'Сделать главным изображением',
+        'reorder_button' => 'Упорядочить',
+        'title_label' => 'Название проекта',
+        'description_label' => 'Описание проекта',
+        'front_label' => 'Главное изображение',
+        'tab' => 'Каталог проектов',
+        'access_permission' => 'Настройки доступа',
+        'manage_projects' => 'Управлять проектами',
+    ],
+    'messages' => [
+        'set_front' => 'Сделать главным изображением проекта?',
+    ],
+    'relation' => [
+        'create_name' => 'Завести новое изображение',
+        'delete' => 'Удалить',
+        'delete_confirm' => 'Удалить изображение?',
+    ],
+    'errors' => [
+        'project_not_found' => 'Проект не найден',
+        'cant_find_selected' => 'Выбранное фото не найдено',
+        'not_this_project' => 'Не в этом проекте',
+    ],
+    'components' => [
+        'projects_list' => 'Список проектов',
+        'projects_list_description' => 'Каталог выполненных проектов',
+    ],
+];
